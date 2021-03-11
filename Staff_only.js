4d01e69d5d4d2854b1498ed9dca3e01c385528c9
Wilkisan <71766893+Wilkisan@users.noreply.github.com>
2021-03-11 12:49:41 +0300

    Update Staff_only.js

--- Staff_only.js
+++ Staff_only.js
@@ -1,57 +1,57 @@
-// == UserScript ==
-// @name "Только персонал"
-// @ пространство имен http://tampermonkey.net/
-// @ версия 0.2
-// @описание !
-// @author You
-// @ матч https://www.rusprofile.ru/*
-// @grant none
-// == / UserScript ==
+// ==UserScript==
+// @name         "Staff only"
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  !
+// @author       You
+// @match        https://www.rusprofile.ru/*
+// @grant        none
+// ==/UserScript==
 
-let inpSerch = document.getElementsByName ("запрос") [0];
-пусть btnSerh = document.querySelector (". search-btn");
-let kWords = ["Антонова Галина Николаевна", "Аткельтирова Гузель Булатовна", "Антохин Александр Николаевич"];
-let capcha = document.querySelector ('. recaptcha-checkbox-border');
-пусть i = 0;
-пусть num = 2;
-пусть совпадение = 0;
+let inpSerch = document.getElementsByName("query")[0];
+let btnSerh = document.querySelector(".search-btn");
+let kWords = ["Антонова Галина Николаевна","Аткельтирова Гузель Булатовна","Антохин Александр Николаевич"];
+let capcha = document.querySelector('.recaptcha-checkbox-border');
+let i = 0;
+let num = 2;
+let match = 0;
 
 
-function getCookie (name) {
+function getCookie(name) {
     let match = document.cookie
-    .расколоть(';')
-    .find (row => row.startsWith (`$ {name} =`));
-    ответный матч? match.split ('=') [1]: не определено;
+    .split(';')
+    .find(row => row.startsWith(`${name}=`));
+    return match ? match.split('=')[1] : undefined;
 }
-if (btnSerh! = null && num == getCookie (nameCoo)) {
-    число = + (getCookie (nameCoo)) + 1;
+if(btnSerh != null && num == getCookie(nameCoo)) {
+    num = +(getCookie(nameCoo)) + 1;
 }
 
-пусть kWord = kWords [число];
+let kWord=kWords[num];
 
-if (btnSerh! = null) {
-    let timerId = setInterval (() => {
-        inpSerch.value + = kWord [i ++];
+if (btnSerh != null) {
+    let timerId = setInterval(()=> {
+        inpSerch.value += kWord[i++];
         if (i == kWord.length) {
-            clearInterval (timerId);
-            document.querySelector (". search-btn"). click ();
+            clearInterval(timerId);
+            document.querySelector(".search-btn").click();
         }
     }, 200);
-} еще {
-    пусть dataTags = document.getElementsByTagName ("а");
-    for (i = 0; i <dataTags.length; i ++) {
-        if (dataTags [i] .outerText == 'ИП' + kWord) {
-            dataTags [i] [0] .click ();
-            document.cookie = `nameCoo = $ {число}`;
-            console.log (кВторд);
+} else {
+    let dataTags = document.getElementsByTagName("a");
+    for (i = 0; i < dataTags.length; i++) {
+        if (dataTags[i].outerText == 'ИП '+ kWord) {
+            dataTags[i][0].click();
+            document.cookie = `nameCoo=${num}`;
+            console.log(kWord);
             location.href = "https://www.rusprofile.ru/";
-            перемена;
-        } if (document.querySelector (". warning-text"). outerText == "ИП ликвидирован") {
+            break;
+        }if (document.querySelector(".warning-text").outerText == "ИП ликвидирован") {
             location.href = "https://www.rusprofile.ru/";
-        if (document.querySelector ('. emptyresult'). outerText.startsWith ("Только действующие") == true) {
+        if (document.querySelector('.emptyresult').outerText.startsWith("Только действующие") == true){
             location.href = "https://www.rusprofile.ru/";
-        } if (capcha == true) {
-            document.querySelector ('. recaptcha-checkbox-border'). click ();
+        }if (capcha == true) {
+            document.querySelector('.recaptcha-checkbox-border').click();
         }
     }
 }
